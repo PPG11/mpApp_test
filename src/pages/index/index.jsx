aa7102b96a8bf38b3df180341ba0088dd8f8b69d
Luolie - MBP 19 <luolie0577@gmail.com>
2021-05-26 11:53:13 +0800

    feat(init): init the repo

---
+++ src/pages/index/index.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react'
+import { View, Text } from '@tarojs/components'
+import './index.less'
+
+export default class Index extends Component {
+  render () {
+    return (
+      <View className='index'>
+        <Text>你好呀</Text>
+      </View>
+    )
+  }
+}
